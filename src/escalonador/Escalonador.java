192dc67a3757d15b975594b9433eff8d2da48f38
T <T@Carlos_Ramon>
2016-02-21 19:51:57 -0300

    Ajustes na Main

--- src/escalonador/Escalonador.java
+++ src/escalonador/Escalonador.java
@@ -9,6 +9,7 @@ import InputFile.InputFileAlg;
 import InputFile.SimpleInputFileFactory;
 import OutputFile.AbstractOutputFile;
 import TaskGenerator.TaskSet;
+import static escalonador.ControllerSingleton.getInstance;
 import java.io.IOException;
 import java.util.Map;
 import java.util.Scanner;
@@ -24,44 +25,9 @@ public class Escalonador {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-        //ControllerSingleton contr = ControllerSingleton.getInstance();
+        ControllerSingleton cont = getInstance() ;
         
-        
-        
-        
-        
-        
-        
-        
-        /*SimpleInputFileFactory factory = new SimpleInputFileFactory();
-        InputFileAlg input = factory.createInputFile(Constants.getTaskGeneratorAlgBasic());
-        Map mapInput;
-        
-        
-          XMLOutputter xout = new XMLOutputter();
-        
-        XStream stream = new XStream(new DomDriver());
-         stream.toXML(inicio);
-        
-        System.out.println("value: " +elemento.getValue() + " text: "+ elemento);
-        String xml = xstream.toXML(arquivo);
-        System.out.println(xml);
-        //xout.output(elemento, System.out); 
-        
-        
-        
-        
-        input.readValues("InputFileTeste.xml");
-        input.normalizeValues();
-        mapInput = input.paramTaskGenerator();
-        System.out.println("amount: "+ mapInput.get("amount"));
-        System.out.println("taskAmountMax: "+ mapInput.get("maxAmountTask"));
-        System.out.println("minAmounTask: "+ mapInput.get("minAmountTask"));
-        System.out.println("utilMax: "+ mapInput.get("utilMax"));*/
-        
-        /*AbstractOutputFile ab = null;
-        TaskSet ts = new TaskSet();
-        ab.setListOutput(ts.getTaskAlgs());*/
+        cont.iniciarPrograma();
     }
     
 }
